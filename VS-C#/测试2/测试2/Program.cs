90ded9acde7ea1affab9d920574baf1a73660996
kycnb666 <85140774+kycnb666@users.noreply.github.com>
2021-12-02 18:29:23 +0800

    Add files via upload

---
+++ VS-C#/测试2/测试2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+using System.Threading;
+
+namespace automaticshutdown
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            while (true)
+            {
+                DateTime time = DateTime.Now;
+                Thread.Sleep(500);
+                if (time.ToString("Hmm") == "1153")
+                {
+                    System.Diagnostics.Process.Start("cmd /c" + "shutdown /s /t 30");
+                    Environment.Exit(0);
+                }else if (time.ToString("Hmm")=="2215")
+                {
+                    System.Diagnostics.Process.Start("cmd /c" + "shutdown /s /t 30");
+                    Environment.Exit(0);
+                }
+
+                
+            }
+        }
+    }
+}
