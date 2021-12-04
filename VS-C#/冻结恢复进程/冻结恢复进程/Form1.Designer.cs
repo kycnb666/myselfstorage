756d7582be44477723aecbfb3b0a898522e3bc72
kycnb666 <85140774+kycnb666@users.noreply.github.com>
2021-12-04 19:07:55 +0800

    Add files via upload

---
+++ VS-C#/冻结恢复进程/冻结恢复进程/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace 冻结恢复进程
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(14, 344);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "冻结";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(189, 344);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "恢复";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(14, 38);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(241, 298);
+            this.listBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.Location = new System.Drawing.Point(85, 344);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "刷新进程列表";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "已读取到     个进程";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(213, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "说明";
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label2_MouseMove);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 388);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "进程冻结与恢复";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ VS-C#/冻结恢复进程/冻结恢复进程/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace 冻结恢复进程
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static class ProcessMgr
+        {
+            [Flags]
+            public enum ProcessAccess : uint
+            {
+                Terminate = 0x1,
+                CreateThread = 0x2,
+                SetSessionId = 0x4,
+                VmOperation = 0x8,
+                VmRead = 0x10,
+                VmWrite = 0x20,
+                DupHandle = 0x40,
+                CreateProcess = 0x80,
+                SetQuota = 0x100,
+                SetInformation = 0x200,
+                QueryInformation = 0x400,
+                SetPort = 0x800,
+                SuspendResume = 0x800,
+                QueryLimitedInformation = 0x1000,
+                Synchronize = 0x100000
+            }
+
+            [DllImport("ntdll.dll")]
+            private static extern uint NtResumeProcess([In] IntPtr processHandle);
+
+            [DllImport("ntdll.dll")]
+            private static extern uint NtSuspendProcess([In] IntPtr processHandle);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            private static extern IntPtr OpenProcess(
+                ProcessAccess desiredAccess,
+                bool inheritHandle,
+                int processId);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool CloseHandle([In] IntPtr handle);
+
+            public static void SuspendProcess(int processId)
+            {
+                IntPtr hProc = IntPtr.Zero;
+                try
+                {
+                    hProc = OpenProcess(ProcessAccess.SuspendResume, false, processId);
+                    if (hProc != IntPtr.Zero)
+                        NtSuspendProcess(hProc);
+                }
+                finally
+                {
+                    if (hProc != IntPtr.Zero)
+                        CloseHandle(hProc);
+                }
+            }
+
+            public static void ResumeProcess(int processId)
+            {
+                IntPtr hProc = IntPtr.Zero;
+                try
+                {
+                    hProc = OpenProcess(ProcessAccess.SuspendResume, false, processId);
+                    if (hProc != IntPtr.Zero)
+                        NtResumeProcess(hProc);
+                }
+                finally
+                {
+                    if (hProc != IntPtr.Zero)
+                        CloseHandle(hProc);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1) { MessageBox.Show("请先选择你要冻结的程序！", "未选择", MessageBoxButtons.OK, MessageBoxIcon.Information); }
+            else
+            {
+                Process[] pros = Process.GetProcessesByName(listBox1.SelectedItem.ToString());
+                DialogResult msg = MessageBox.Show($"确定要冻结{listBox1.SelectedItem.ToString()}吗？", "请确认", MessageBoxButtons.YesNo);
+                if (msg == DialogResult.Yes)
+                    ProcessMgr.SuspendProcess(pros[0].Id);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1) { MessageBox.Show("请先选择你要恢复的程序！", "未选择", MessageBoxButtons.OK, MessageBoxIcon.Information); }
+            else
+            {
+                Process[] pros = Process.GetProcessesByName(listBox1.SelectedItem.ToString());
+                ProcessMgr.ResumeProcess(pros[0].Id);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach(Process proc in processes)
+            {
+                if(proc.ProcessName!="svchost")
+                listBox1.Items.Add(proc.ProcessName);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int i=0;
+            Process[] processes = Process.GetProcesses();
+            foreach (Process proc in processes)
+            {
+                if (proc.ProcessName != "svchost")
+                    ++i;
+            }label1.Text= $"已读取到{i.ToString()}个进程";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process proc in processes)
+            {
+                if (proc.ProcessName != "svchost")
+                    listBox1.Items.Add(proc.ProcessName);
+            }
+        }
+
+        private void label2_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip1.SetToolTip(this.label2, "请先确认你要冻结的程序再继续！\n谨慎操作，防止冻到系统进程！");
+        }
+    }
+}
