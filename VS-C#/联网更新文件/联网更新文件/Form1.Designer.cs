cb91f58d4bd4390d65d0f8422593460b050334ee
kycnb666 <85140774+kycnb666@users.noreply.github.com>
2021-12-25 19:07:53 +0800

    Add files via upload

--- VS-C#/联网更新文件/联网更新文件/Form1.Designer.cs
+++ VS-C#/联网更新文件/联网更新文件/Form1.Designer.cs
@@ -29,29 +29,122 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(46, 46);
+            this.button1.Location = new System.Drawing.Point(65, 24);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 74);
+            this.button1.Size = new System.Drawing.Size(147, 113);
             this.button1.TabIndex = 0;
             this.button1.Text = "检查更新";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 113);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "检查更新";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(237, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(162, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(475, 399);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "选择节点进行更新";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(134, 83);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(165, 98);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "检查更新";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox2.Location = new System.Drawing.Point(238, 48);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(193, 29);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Gitee节点（国内）";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox1.Location = new System.Drawing.Point(26, 48);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(210, 29);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "GitHub节点（国外）";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(239, 181);
+            this.ClientSize = new System.Drawing.Size(499, 448);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -59,6 +152,13 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- VS-C#/联网更新文件/联网更新文件/Form1.cs
+++ VS-C#/联网更新文件/联网更新文件/Form1.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 using System.Net;
 using System.IO;
 using System.Diagnostics;
+using System.Web;
+using System.Threading;
+
 
 namespace 联网更新文件
 {
@@ -18,51 +21,102 @@ namespace 联网更新文件
         public Form1()
         {
             InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        void DownloadFileFromGitHub()
         {
             try
             {
-                
-
-
                 WebClient webClient = new WebClient();
                 webClient.Encoding = Encoding.UTF8;
-                Application.DoEvents();
-                webClient.DownloadFile("https://raw.githubusercontent.com/kycnb666/softwarerelease/main/software%20release/%E6%96%87%E6%9C%AC%E5%8A%A0%E5%AF%86%E8%A7%A3%E5%AF%86.exe", $"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe");
-                
+                webClient.DownloadFile("https://github.com/kycnb666/softwarerelease/raw/main/software%20release/%E8%81%94%E7%BD%91%E6%9B%B4%E6%96%B0%E6%96%87%E4%BB%B6.exe", $"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe");
+                button5.Enabled = true;
+                button5.Text = "检查更新";
                 System.Diagnostics.FileVersionInfo newversion = System.Diagnostics.FileVersionInfo.GetVersionInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe");
 
                 string thisversion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
                 if (newversion.FileVersion != thisversion)
                 {
-                    DialogResult msg= MessageBox.Show("检测到程序有新版本！\n是否更新？","有新版本啦！",MessageBoxButtons.YesNo,MessageBoxIcon.Information);
-                    if(msg == DialogResult.Yes)
+                    DialogResult msg = MessageBox.Show("检测到程序有新版本！\n是否更新？", "有新版本啦！", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (msg == DialogResult.Yes)
                     {
                         MessageBox.Show("更新完成，请重启本程序以完成更新");
                     }
                     else
                     {
-                        FileInfo f=new FileInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe");
+                        FileInfo f = new FileInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe");
                         f.Delete();
                     }
                 }
-                else if(newversion.FileVersion == thisversion)
+                else if (newversion.FileVersion == thisversion)
                 {
                     FileInfo f = new FileInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe");
                     f.Delete();
-                    MessageBox.Show("当前已是最新版本！");
+                    MessageBox.Show("当前已是最新版本！", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
 
 
             }
-            catch (Exception ex) { MessageBox.Show("检查失败！可能原因：\n1.电脑无网络连接\n2.服务器更新节点无响应\n3.详细错误信息如下：\n\n\n"+ex.ToString()); }
+            catch (Exception)
+            {
+                button1.Text = "检查更新";
+                button1.Enabled = true;
+                MessageBox.Show("检查失败！\n无法与服务器建立连接！\n解决办法：\n重试几次或者稍后再试！", "检查失败！", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+        void DownloadFileFromGitee()
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                webClient.DownloadFile("https://gitee.com/kycnb666/softwarerelease/raw/master/software%20release/%E8%81%94%E7%BD%91%E6%9B%B4%E6%96%B0%E6%96%87%E4%BB%B6.exe", $"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe");
+                button5.Enabled = true;
+                button5.Text = "检查更新";
+                System.Diagnostics.FileVersionInfo newversion = System.Diagnostics.FileVersionInfo.GetVersionInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe");
 
-        private void button2_Click(object sender, EventArgs e)
-        { }
+                string thisversion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+                if (newversion.FileVersion != thisversion)
+                {
+                    DialogResult msg = MessageBox.Show("检测到程序有新版本！\n是否更新？", "有新版本啦！", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (msg == DialogResult.Yes)
+                    {
+                        MessageBox.Show("更新完成，请重启本程序以完成更新");
+                    }
+                    else
+                    {
+                        FileInfo f = new FileInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe");
+                        f.Delete();
+                    }
+                }
+                else if (newversion.FileVersion == thisversion)
+                {
+                    FileInfo f = new FileInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe");
+                    f.Delete();
+                    MessageBox.Show("当前已是最新版本！","",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                }
+
+
+            }
+            catch (Exception)
+            {
+                button5.Text = "检查更新";
+                button5.Enabled = true;
+                MessageBox.Show("检查失败！\n无法与服务器建立连接！\n解决办法：\n重试几次或者稍后再试！", "检查失败！", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread download=new Thread(new ThreadStart(DownloadFileFromGitHub));
+            download.Start();
+            button1.Enabled = false;
+            button1.Text = "正在检查，请稍后...";
+        } 
+             
+
+        
 
         private void Form1_Load(object sender, EventArgs e)
         { }
@@ -71,6 +125,103 @@ namespace 联网更新文件
         {
             if(File.Exists($"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe"))
             Process.Start("cmd.exe", $"/k rename \"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.exe\" \"{Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().Location)}\"");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread download = new Thread(new ThreadStart(DownloadFileFromGitee));
+            download.Start();
+            button5.Enabled = false;
+            button5.Text = "正在检查，请稍后...";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            HttpDownloadFile("https://github.com/kycnb666/softwarerelease/raw/main/software%20release/%E6%96%87%E6%9C%AC%E5%8A%A0%E5%AF%86%E8%A7%A3%E5%AF%86.exe", "");
+        }
+        
+        public static string HttpDownloadFile(string url, string path)
+        {
+            // 设置参数
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            //发送请求并获取相应回应数据
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            //直到request.GetResponse()程序才开始向目标网页发送Post请求
+            Stream responseStream = response.GetResponseStream();
+
+            //创建本地文件写入流
+            Stream stream = new FileStream(path, FileMode.Create);
+
+            byte[] bArr = new byte[1024];
+            int size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            while (size > 0)
+            {
+                stream.Write(bArr, 0, size);
+                size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            }
+            stream.Close();
+            responseStream.Close();
+            return path;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            Application.DoEvents();
+            webClient.DownloadFile("https://github.com/kycnb666/softwarerelease/blob/main/software%20release/%E6%96%87%E6%9C%AC%E5%8A%A0%E5%AF%86%E8%A7%A3%E5%AF%86.exe", $"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.html");
+
+            StreamReader streamReader = new StreamReader($"{AppDomain.CurrentDomain.BaseDirectory}\\linshi.html");
+            string a = streamReader.ReadToEnd();
+           
+            streamReader.Close();
+            string b = "文本加密解密.exe";
+            
+            bool isContains = a.ToLower().Contains(b.ToLower());//true
+            MessageBox.Show(isContains.ToString());
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true || checkBox2.Checked == true)
+            {
+                if (checkBox1.Checked)
+                {
+                    Thread download = new Thread(new ThreadStart(DownloadFileFromGitHub));
+                    download.Start();
+                    button5.Enabled = false;
+                    button5.Text = "正在检查，请稍后...";
+                }
+                else if (checkBox2.Checked)
+                {
+                    Thread download = new Thread(new ThreadStart(DownloadFileFromGitee));
+                    download.Start();
+                    button5.Enabled = false;
+                    button5.Text = "正在检查，请稍后...";
+                }
+            }
+            else
+            {
+                MessageBox.Show("请选择下载节点！", "未选择下载节点", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+                checkBox1.Checked = false;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox2.Checked)
+                checkBox2.Checked = false; 
         }
     }
 }
