8701d19dcea98f8b1b04f121bfb9c8717a08f15b
noob programmer <85140774+kycnb666@users.noreply.github.com>
2022-06-22 18:16:31 +0800

    Add files via upload

--- VS-C#/在线工具箱/在线工具箱/Properties/Resources.Designer.cs
+++ VS-C#/在线工具箱/在线工具箱/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace 在线工具箱.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap _360qdds {
+            get {
+                object obj = ResourceManager.GetObject("360qdds", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap aida64 {
             get {
                 object obj = ResourceManager.GetObject("aida64", resourceCulture);
